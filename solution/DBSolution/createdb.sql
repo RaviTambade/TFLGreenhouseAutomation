84bb4140b64193a4852428c280335eb606782626
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2024-02-26 14:48:50 +0530

    stored procedure in event

--- solution/DBSolution/createdb.sql
+++ solution/DBSolution/createdb.sql
@@ -1,15 +1,36 @@
 
+
 drop database If exists TFLGreenHouse;
 create database  TFLGreenHouse;
 use TFLGreenHouse;
-create table temperaturesreadings(id Int auto_increment primary key, time datetime DEFAULT NOW(),internal int ,external int);
+create table temperaturesreadings(id Int auto_increment primary key, time datetime DEFAULT NOW(),internal int ,external int); 
+
+
+
+
+DELIMITER $$
+
+CREATE PROCEDURE sptemperaturesreadings()
+BEGIN
+	INSERT INTO temperaturesreadings(internal,external)
+  VALUES(RAND()*(10-5)+5,RAND()*(10-5)+5);
+
+END$$
+DELIMITER ;
+ 
+ 
  
 CREATE EVENT IF NOT EXISTS temperature_reader_event
 ON SCHEDULE EVERY 10 second
 STARTS CURRENT_TIMESTAMP
+Ends CURRENT_TIMESTAMP + interval 5 minute
 DO
-  INSERT INTO temperaturesreadings(internal,external)
-  VALUES(RAND()*(10-5)+5,RAND()*(10-5)+5);
+Call sptemperaturesreadings();
 
-select * from  temperaturesreadings;
-SHOW EVENTS FROM TFLGreenHouse;
+  
+  
+ 
+  
+  select * from  temperaturesreadings;
+  
+  SHOW EVENTS FROM TFLGreenHouse;
