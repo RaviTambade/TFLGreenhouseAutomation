f26811c8b5556d838bf4820c5aeae35f33934b4e
KaustubhPatil10 <kaustubhpatil331@gmail.com>
2024-10-22 17:32:26 +0530

    creaddb

---
+++ solution/DBSolution/MsSQL/createdb.sql
@@ -0,0 +1,34 @@
+﻿-- creating a database tflgreenhouseautomation
+CREATE DATABASE TFLGreenhouseAutomation;
+
+-- use the database
+USE TFLGreenhouseAutomation;
+
+-- creating a table users
+CREATE TABLE Users (
+	UserID INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(50) NOT NULL,
+    PasswordHash NVARCHAR(255) NOT NULL,
+    Role NVARCHAR(20) NOT NULL, -- e.g., Admin, User
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
+
+-- creating a table Sensors
+CREATE TABLE Sensors (
+	SensorID INT PRIMARY KEY IDENTITY(1,1),
+    SensorType NVARCHAR(50) NOT NULL, -- e.g., Temperature, Humidity
+    Location NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(255),
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
+
+-- creating a table SensorReadings
+CREATE TABLE SensorReadings (
+	ReadingID INT PRIMARY KEY IDENTITY(1,1),
+    SensorID INT FOREIGN KEY REFERENCES Sensors(SensorID),
+    ReadingValue FLOAT NOT NULL,
+    ReadingTime DATETIME DEFAULT GETDATE()
+);
+
+
+
