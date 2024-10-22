b832866241dcd7ac5f7f7daa9643ade07426babc
PranavT20 <pranavtaware2001@gmail.com>
2024-10-22 17:58:28 +0530

    sampledb

--- solution/DBSolution/MsSQL/Joindb.sql
+++ solution/DBSolution/MsSQL/Joindb.sql
@@ -3,12 +3,14 @@
 -- 1.Retrive all users
 	SELECT * from users;
  
+
 -- 2.Retrive Sensor Information with Readings
 	SELECT s.SensorID, s.SensorType, sr.ReadingValue, sr.ReadingTime
 	FROM Sensors s
 	JOIN SensorReadings sr ON s.SensorID =sr.SensorID
 	ORDER BY sr.ReadingTime DESC;
  
+
 -- 3.Get Recent Sensor Readings for a Specific Sensor Type
 SELECT sr.ReadingID,sr.ReadingValue,s.SensorType
 FROM SensorReadings sr
@@ -16,9 +18,11 @@ JOIN Sensors S ON sr.SensorID=s.SensorID
 WHERE s.SensorType='Humidity'
 ORDER BY sr.ReadingValue DESC;
  
+
 -- 4.Get Control Device Status
 SELECT cd.DeviceID,cd.DeviceType,cd.Status
 FROM ControlDevices cd;
+
 
 -- 5.Get Control logs for a specific Device
 SELECT cl.LogID,cd.DeviceType,cl.Action,cl.ActionTime
@@ -39,22 +43,44 @@ WHERE sr.ReadingValue < st.ThresholdMin OR sr.ReadingValue > st.ThresholdMax;
   FROM SensorReadings sr 
   Join Sensors s ON sr.SensorID = s.SensorID
   JOIN Settings st ON s.SensorType = st.SensorType
- WHERE sr.ReadingValue < st.ThresholdMin OR sr.ReadingValue > st.ThresholdMax; -- INSERT INTO SensorReadings (SensorID, ReadingValue, ReadingTime) VALUES (1, 40, GETDATE()); 
---7. Count of Sensor Readings per Sensor Types SELECT s.SensorType, COUNT(sr.ReadingID) AS Reading_count FROM SensorReadings sr JOIN Sensors s ON sr.SensorID = s.SensorID GROUP BY s.SensorType;
+ WHERE sr.ReadingValue < st.ThresholdMin OR sr.ReadingValue > st.ThresholdMax;
+
+
+-- 7. Count of Sensor Readings per Sensor Types 
+  SELECT s.SensorType, COUNT(sr.ReadingID) AS Reading_count 
+  FROM SensorReadings sr 
+  JOIN Sensors s ON sr.SensorID = s.SensorID 
+  GROUP BY s.SensorType;
+
  --8. Get users with Admin Role. 
 SELECT * FROM users WHERE role= 'Admin'; 
+
+
 --9. Get Devices that are currently On 
 SELECT * 
 FROM ControlDevices
- WHERE status = 'on'; 
+WHERE status = 'on';
+
+
 --10. Recent actions taken by Control Devices 
 SELECT cd.DeviceID, cd.DeviceType, cl.Action, cl.ActionTime
- FROM ControlLogs cl
-  JOIN ControlDevices cd ON cl.DeviceID = cd.DeviceID 
-  ORDER BY cl.ActionTime DESC; 
+FROM ControlLogs cl
+JOIN ControlDevices cd ON cl.DeviceID = cd.DeviceID 
+ORDER BY cl.ActionTime DESC; 
+
+
 --11. Retrieve Settings for Sensor Types.
  SELECT * FROM Settings; 
+
+
 --12. Average Sensor Reading for Temperature.
  SELECT AVG(ReadingValue) AS Average_temp
  FROM SensorReadings
  WHERE SensorID = (SELECT SensorID FROM Sensors WHERE SensorType = 'Temperature');
+
+
+--13.Find latest Reading for each Sensor.
+SELECT s.SensorID, s.SensorType, sr.ReadingValue, sr.ReadingTime
+FROM Sensors s
+JOIN SensorReadings sr ON s.SensorID =sr.SensorID
+WHERE sr.ReadingTime = (SELECT MAX(ReadingTime) FROM SensorReadings WHERE SensorID = s.SensorID);
--- solution/DBSolution/MsSQL/sampledb.sql
+++ solution/DBSolution/MsSQL/sampledb.sql
@@ -17,3 +17,30 @@ INSERT INTO SensorReadings (SensorID, ReadingValue, ReadingTime) VALUES
 (3, 30.0, GETDATE()),  -- Soil moisture reading
 (4, 500, GETDATE()),    -- Light intensity reading
 (1, 40, GETDATE());
+
+-- Inserting into ControlDevices
+
+INSERT INTO ControlDevices (DeviceType, Status, Location) VALUES 
+
+('Water Pump', 'Off', 'Greenhouse 1'),
+('Heater', 'On', 'Greenhouse 1'),
+('Fan', 'Off', 'Greenhouse 1'),
+('LED Grow Light', 'On', 'Greenhouse 1');
+ 
+-- Inserting into Settings
+
+INSERT INTO Settings (SensorType, ThresholdMin, ThresholdMax, NotificationEnabled) VALUES 
+
+('Temperature', 18.0, 28.0, 1),  -- Alerts if out of range
+('Humidity', 40.0, 70.0, 1),     -- Alerts if out of range
+('Soil Moisture', 20.0, 50.0, 1), -- Alerts if out of range
+('Light', 200, 800, 1);            -- Alerts if out of range
+ 
+ -- Insert into ControlLogs
+ 
+INSERT INTO ControlLogs(DeviceID,Action,ActionTime) VALUES 
+
+(1,'Started',GETDATE()),
+(2,'Stopped',GETDATE()),
+(3,'Started',GETDATE()),
+(4,'Stopped',GETDATE());
